1512831799e4aac7137acc1910a65ee89f0ee550
Kevin Walsh <kwalsh@holycross.edu>
2024-07-05 21:20:42 -0400

    dead code

--- src/com/cburch/logisim/util/SocketClient.java
+++ src/com/cburch/logisim/util/SocketClient.java
@@ -29,110 +29,112 @@
  */
 package com.cburch.logisim.util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.io.PrintWriter;
+// import java.net.ServerSocket;
+// import java.net.Socket;
 
 /**
  * Socket client to talk to the binder.
  *
  * @author christian.mueller@heig-vd.ch
  */
-public class SocketClient {
 
-  private static ServerSocket server = null;
-
-  private boolean connected = false;
-
-  private Socket socket;
-
-  private BufferedReader socket_reader;
-  private PrintWriter socket_writer;
-
-  public SocketClient() {
-
-    if (server == null) {
-      try {
-        server = new ServerSocket(0);
-      } catch (IOException e) {
-        System.err.println("Cannot create server socket");
-        e.printStackTrace();
-        return;
-      }
-    }
-  }
-
-  public int getServerPort() {
-    if (server != null) {
-      return server.getLocalPort();
-    }
-
-    return 0;
-  }
-
-  public Socket getSocket() {
-    return socket;
-  }
-
-  public Boolean isConnected() {
-    return connected;
-  }
-
-  public String receive() {
-
-    try {
-      return socket_reader.readLine();
-    } catch (Exception e) {
-      System.err.printf("Cannot read from socket : %s\n", e.getMessage());
-      return null;
-    }
-  }
-
-  public void send(String message) {
-
-    try {
-      socket_writer.println(message);
-    } catch (Exception e) {
-      System.err.printf("Cannot write %s to socket %s\n", message,
-          e.getMessage());
-    }
-  }
-
-  public void start() {
-
-    try {
-      socket = server.accept();
-
-      socket_reader = new BufferedReader(new InputStreamReader(
-            socket.getInputStream()));
-
-      socket_writer = new PrintWriter(new BufferedWriter(
-            new OutputStreamWriter(socket.getOutputStream())), true);
-
-      connected = true;
-      return;
-
-    } catch (IOException e) {
-      System.err.println("Error at accepting new client");
-    }
-    connected = false;
-  }
-
-  public void stop() {
-    if (!isConnected())
-      return;
-
-    try {
-      socket.close();
-      connected = false;
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-}
+// This code is disabled as it is not maintained and is used only by dead code.
+public class SocketClient { }
+// 
+//   private static ServerSocket server = null;
+// 
+//   private boolean connected = false;
+// 
+//   private Socket socket;
+// 
+//   private BufferedReader socket_reader;
+//   private PrintWriter socket_writer;
+// 
+//   public SocketClient() {
+// 
+//     if (server == null) {
+//       try {
+//         server = new ServerSocket(0);
+//       } catch (IOException e) {
+//         System.err.println("Cannot create server socket");
+//         e.printStackTrace();
+//         return;
+//       }
+//     }
+//   }
+// 
+//   public int getServerPort() {
+//     if (server != null) {
+//       return server.getLocalPort();
+//     }
+// 
+//     return 0;
+//   }
+// 
+//   public Socket getSocket() {
+//     return socket;
+//   }
+// 
+//   public Boolean isConnected() {
+//     return connected;
+//   }
+// 
+//   public String receive() {
+// 
+//     try {
+//       return socket_reader.readLine();
+//     } catch (Exception e) {
+//       System.err.printf("Cannot read from socket : %s\n", e.getMessage());
+//       return null;
+//     }
+//   }
+// 
+//   public void send(String message) {
+// 
+//     try {
+//       socket_writer.println(message);
+//     } catch (Exception e) {
+//       System.err.printf("Cannot write %s to socket %s\n", message,
+//           e.getMessage());
+//     }
+//   }
+// 
+//   public void start() {
+// 
+//     try {
+//       socket = server.accept();
+// 
+//       socket_reader = new BufferedReader(new InputStreamReader(
+//             socket.getInputStream()));
+// 
+//       socket_writer = new PrintWriter(new BufferedWriter(
+//             new OutputStreamWriter(socket.getOutputStream())), true);
+// 
+//       connected = true;
+//       return;
+// 
+//     } catch (IOException e) {
+//       System.err.println("Error at accepting new client");
+//     }
+//     connected = false;
+//   }
+// 
+//   public void stop() {
+//     if (!isConnected())
+//       return;
+// 
+//     try {
+//       socket.close();
+//       connected = false;
+//     } catch (IOException e) {
+//       e.printStackTrace();
+//     }
+//   }
+// 
+// }
